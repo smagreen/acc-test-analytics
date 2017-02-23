b1c91226494fca75048f1eb965c62e2f2d453b8a
Steve Green <home@steve-green.com>
2017-02-23 20:57:25 +0000

    Capability service (forkJoin) simplified

--- src/app/capability/capability.service.ts
+++ src/app/capability/capability.service.ts
@@ -8,12 +8,12 @@ import { Subject } from 'rxjs/Subject';
 
 
 const CAPABILITIES: ICapability[] = [
-    { id: '11', attributeId: '1', componentId: '1', name: 'Capability A1 - C1', description: ''},
+    { id: '11', attributeId: '1', componentId: '1', name: 'Capability A1 - C1', description: 'This is the description for Capability id:11'},
     { id: '112', attributeId: '1', componentId: '1', name: 'Capability 2 A1 - C1', description: ''},
     { id: '12', attributeId: '1', componentId: '2', name: 'Capability A1 - C2', description: ''},
     { id: '13', attributeId: '1', componentId: '3', name: 'Capability A1 - C3', description: ''},
     { id: '21', attributeId: '2', componentId: '1', name: 'Capability A2 - C1', description: ''},
-    { id: '22', attributeId: '2', componentId: '2', name: 'Capability A2 - C2', description: ''},
+    { id: '22', attributeId: '2', componentId: '2', name: 'Capability A2 - C2', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.'},
     { id: '24', attributeId: '2', componentId: '4', name: 'Capability A2 - C4', description: ''},
     { id: '64', attributeId: '6', componentId: '4', name: 'Capability A6 - C4', description: ''},
     { id: '65', attributeId: '6', componentId: '4', name: 'Capability A6 - C4', description: ''},
@@ -21,10 +21,10 @@ const CAPABILITIES: ICapability[] = [
     { id: '26', attributeId: '2', componentId: '6', name: 'Capability A2 - C6', description: ''},
     { id: '33', attributeId: '2', componentId: '6', name: 'Capability 2 A2 - C6', description: ''},
     { id: '34', attributeId: '2', componentId: '6', name: 'Capability 3 A2 - C6', description: ''},
-    { id: '35', attributeId: '2', componentId: '6', name: 'Capability 4 A2 - C6', description: ''},
+    { id: '35', attributeId: '2', componentId: '6', name: 'Capability 4 A2 - C6', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.'},
     { id: '36', attributeId: '2', componentId: '6', name: 'Capability 5 A2 - C6', description: ''},
     { id: '37', attributeId: '2', componentId: '6', name: 'Capability 6 A2 - C6', description: ''},
-    { id: '38', attributeId: '2', componentId: '6', name: 'Capability 7 A2 - C6', description: ''}
+    { id: '38', attributeId: '2', componentId: '6', name: 'Capability 7 A2 - C6', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.'}
 ];
 
 @Injectable()
@@ -43,11 +43,17 @@ export class CapabilityService {
     }
 
      getCapabilityData(): Observable<CapabilityMatrix> {
+
        return Observable.forkJoin(
-        this.getCapabilities().map((caps: ICapability[]) => caps ),
-        this.componentService.getComponents().map((comps: IComponent[]) => comps ),
-        this.attributeService.getAttributes().map((attrs: IAttribute[]) => attrs)
+            this.getCapabilities(),
+            this.componentService.getComponents(),
+            this.attributeService.getAttributes()
       ).map(res => this.resolveMatrix( res[2], res[1], res[0]));
+
+    }
+
+    getCapability(id: string): Observable<ICapability> {
+        return Observable.of(CAPABILITIES.find(c => c.id === id));
     }
 
     // build the matrix - resolving attribute and component ids to the relevant objects
